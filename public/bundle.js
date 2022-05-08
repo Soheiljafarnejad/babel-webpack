3fd1587a24a47278679b60c9ece3215b0df3a186
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-05-08 13:17:17 +0430

    babel-loader

--- public/bundle.js
+++ public/bundle.js
@@ -1,1 +1,1 @@
-(new class{getName(e){console.log(`hi ${e}`)}}).getName("soehil");
+(()=>{function e(e,n){for(var t=0;t<n.length;t++){var o=n[t];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}(new(function(){function n(){!function(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}(this,n)}var t,o;return t=n,(o=[{key:"getName",value:function(e){console.log("hi ".concat(e))}}])&&e(t.prototype,o),Object.defineProperty(t,"prototype",{writable:!1}),n}())).getName("soehil")})();
